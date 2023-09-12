e728c8ed0c85fc146fa8e4020dee39f99d3d274d
Rrantisi <rantisireham19@gmail.com>
2023-09-12 11:42:05 -0400

    adds todo model

---
+++ models/todo.js
@@ -0,0 +1,27 @@
+// Todo Model
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+    todo_description: {
+        type: String,
+        required: true
+    },
+    todo_responsible: {
+        type: String,
+        required: true
+    },
+    todo_priority: {
+        type: String,
+        required: true
+    },
+    todo_completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
