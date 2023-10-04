adec6ff96c9ce0e4897e1344994eca83e8e3af29
Rrantisi <rantisireham19@gmail.com>
2023-10-04 10:08:17 -0400

    fixes getTodo and updateTodo

--- controllers/todos.js
+++ controllers/todos.js
@@ -28,7 +28,7 @@ async function index(req, res) {
 async function details(req, res) {
     try {
         const id = req.params.id;
-        const user = await User.findById(req.user._id).populate('todos');
+        const user = await User.findById(req.query.userId).populate('todos');
         const foundTodos = user.todos;
         let todoId, todoInfo;
         foundTodos.forEach(todo => {
